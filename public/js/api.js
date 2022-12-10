4fb5630920e7840e9fc5fed50ab760e225d5dfd6
Alexei Ruiz <alexeiruizrodri@gmail.com>
2022-12-10 11:06:27 -0300

    Utilizando javascript para el body hasta category

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "c18ce25377bdb06be6d28d373f793e8f";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
